d35ab070c04c88ab5a440e8bb0fadbeee5b4f0d5
Redrb <obosalexis@gmail.com>
2021-04-17 00:43:45 -0500

    otro build

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=gZEl6ylhPXruCN6T9aU4a8LYwXbIgeAO`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=gZEl6ylhPXruCN6T9aU4a8LYwXbIgeAO`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map((item) => {
